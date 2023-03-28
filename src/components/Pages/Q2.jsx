bf522318c60dcbdac7bb3e1e777847bc1cce5a87
200747 <200747@student.glu.nl>
2023-03-28 21:30:15 +0200

    eeee

--- src/components/Pages/Q2.jsx
+++ src/components/Pages/Q2.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import arrow from '../../assets/arrow.png'
+import arrow2 from '../../assets/arrow2.png'
 import Navbar from "../Navbar"
 
 
@@ -12,13 +13,14 @@ const Q1 = () => {
             <div className='m-0 text-4xl text-center pt-5 font-medium'>Belang van de kwaliteit van het activiteitenprogramma.</div>
         </div>
 
-        <div className=' flex gap-8 mt-8 ml-[100px] items-center justify-center text-center'>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>1</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>2</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>3</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>4</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>5</div>
-            <div className='w-32 mr-4'><img src={arrow} alt="Logo" /></div> 
+      <div className=' flex gap-8 mt-8 ml-[100px] items-center justify-center text-center'>
+      <a href="/"><div className='w-32'><img src={arrow2} alt="Arrow2" /></div></a>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>1</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>2</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>3</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>4</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>5</div>
+          <div className='w-32 mr-24'><img src={arrow} alt="Arrow" /></div> 
         </div>
       </div>
   );
--- src/components/Q1.jsx
+++ src/components/Q1.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import arrow from '../assets/arrow.png'
-import Navbar from "../components/Navbar"
+import Navbar from "./Navbar"
 
 
 const Q1 = () => {
@@ -13,12 +13,12 @@ const Q1 = () => {
         </div>
 
         <div className=' flex gap-8 mt-8 ml-[100px] items-center justify-center text-center'>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>1</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>2</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>3</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>4</div>
-            <div className='square leading-[110px] w-[8rem] h-[8rem] rounded-xl'>5</div>
-            <div className='w-32 mr-4'><img src={arrow} alt="Logo" /></div> 
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>1</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>2</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>3</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>4</div>
+            <div className='square leading-[110px] w-[12rem] h-[12rem] rounded-xl'>5</div>
+            <a href="Q2"><div className='w-[100%] pr-4'><img src={arrow} alt="Logo" /></div></a>
         </div>
       </div>
   );
