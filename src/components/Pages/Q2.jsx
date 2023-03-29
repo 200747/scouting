91f0fbe0d8163125c9e4824a0d485684afd63b7d
200747 <200747@student.glu.nl>
2023-03-29 22:09:02 +0200

    eeee

--- src/components/Pages/Q2.jsx
+++ src/components/Pages/Q2.jsx
@@ -12,17 +12,19 @@ const Q2 = () => {
             <div className='m-0 text-4xl text-center pt-5'>Kwaliteitsbewaking team en activiteitenprogramma</div>
             <div className='m-0 text-4xl text-center pt-5 font-medium'>Belang van de kwaliteit van het activiteitenprogramma.</div>
         </div>
-
-    <div className='relative'>
-      <div className='float-left mt-16 pr-12 ml-8 mr-8'><a href="/"><img src={arrow2} alt="Arrow2" /></a></div>
+        
+    <div className=''>
+      <div className=''>
+      <div className='float-left mt-16 ml-8 mr-8'><a href="/"><img src={arrow2} alt="Arrow2" /></a></div>
       <div className='float-right mt-16 ml-8 mr-8'><a href="Q3"><img src={arrow} alt="Arrow" /></a></div>
-      <section className='aaa grid grid-cols-5 mt-8 text-center'>
-            <div className='square leading-[160px] w-[11rem] h-[11rem] mt-8 rounded-xl'>1</div>
-            <div className='square leading-[160px] w-[11rem] h-[11rem] mt-8 rounded-xl'>2</div>
-            <div className='square leading-[160px] w-[11rem] h-[11rem] mt-8 rounded-xl'>3</div>
-            <div className='square leading-[160px] w-[11rem] h-[11rem] mt-8 rounded-xl'>4</div>
-            <div className='square leading-[160px] w-[11rem] h-[11rem] mt-8 rounded-xl'>5</div>
-        </section>
+      </div>
+      <div className='aaa grid grid-cols-5 mt-8 text-center'>
+            <div className='square hehe   leading-[160px]  h-[11rem] mt-8 rounded-xl'>1</div >
+            <div className='square hehe   leading-[160px]  h-[11rem] mt-8 rounded-xl'>2</div>
+            <div className='square hehe   leading-[160px]  h-[11rem] mt-8 rounded-xl'>3</div>
+            <div className='square hehe  leading-[160px]  h-[11rem] mt-8 rounded-xl'>4</div>
+            <div className='square hehe  leading-[160px]  h-[11rem] mt-8 rounded-xl'>5</div>
+        </div>
         </div>
     </div>
   );
--- src/components/Q1.jsx
+++ src/components/Q1.jsx
@@ -6,21 +6,26 @@ import Navbar from "./Navbar"
 const Q1 = () => {
   return (
     <div name='home' className=' w-full h-screen'>
-      <Navbar></Navbar>
+          <Navbar></Navbar>
         <div>
-            <div className='m-0 text-4xl text-center pt-5'>Coördinerende en teamgerichte taken</div>
-            <div className='m-0 text-4xl text-center pt-5 font-medium'>Periodiek planning taken en werkzaamheden maken.</div>
+            <div className='m-0 text-4xl text-center pt-5'>Kwaliteitsbewaking team en activiteitenprogramma</div>
+            <div className='m-0 text-4xl text-center pt-5 font-medium'>Belang van de kwaliteit van het activiteitenprogramma.</div>
         </div>
-
-        <div className='flex gap-8 mt-8 ml-[100px] items-center justify-center text-center'>
-            <div className='square leading-[160px] w-[12rem] h-[12rem] rounded-xl'>1</div>
-            <div className='square leading-[160px] w-[12rem] h-[12rem] rounded-xl'>2</div>
-            <div className='square leading-[160px] w-[12rem] h-[12rem] rounded-xl'>3</div>
-            <div className='square leading-[160px] w-[12rem] h-[12rem] rounded-xl'>4</div>
-            <div className='square leading-[160px] w-[12rem] h-[12rem] rounded-xl'>5</div>
-            <a href="Q2"><div className='w-32'><img src={arrow} alt="Logo" /></div></a>
-        </div>
+        
+    <div className=''>
+      <div className=''>
+      <div className='float-left mt-16 ml-8 mr-8 rah'><a href="/"><img src={arrow} alt="Arrow" /></a></div>
+      <div className='float-right mt-16 ml-8 mr-8'><a href="Q2"><img src={arrow} alt="Arrow" /></a></div>
       </div>
+      <div className='aaa grid grid-cols-5 mt-8 gap-2 text-center'>
+            <div className='square hehe   leading-[160px]  h-[11rem] mt-8 rounded-xl'>1</div >
+            <div className='square hehe   leading-[160px]  h-[11rem] mt-8 rounded-xl'>2</div>
+            <div className='square hehe   leading-[160px]  h-[11rem] mt-8 rounded-xl'>3</div>
+            <div className='square hehe  leading-[160px]  h-[11rem] mt-8 rounded-xl'>4</div>
+            <div className='square hehe  leading-[160px]  h-[11rem] mt-8 rounded-xl'>5</div>
+        </div>
+        </div>
+    </div>
   );
 };
 
